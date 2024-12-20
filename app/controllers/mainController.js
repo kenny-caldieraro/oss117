021a6a2555643cf68705cb634e5ba22b1570ef81
kenny <kenny@nematis.com>
2024-12-20 16:35:02 +0100

    add endpoint for slack

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,77 +2,90 @@ const Quotes = require('../models/quotes');
 const { Sequelize } = require('sequelize');
 
 const mainController = {
-  error404(_, res) {
-    res.status(404).render('404');
-  },
+    error404(_, res) {
+        res.status(404).render('404');
+    },
 
-  logUrl(req, _, next) {
-    console.log(req.url);
-    next();
-  },
+    logUrl(req, _, next) {
+        console.log(req.url);
+        next();
+    },
 
-  mainUrl(_, res) {
-    res.render('index', {});
-  },
+    mainUrl(_, res) {
+        res.render('index', {});
+    },
 
-  async randomQuote(_, res) {
-    try {
-      const quote = await Quotes.findOne({
-        order: [Sequelize.fn('RAND')],
-      });
-      res.json(quote);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  },
+    async randomQuote(_, res) {
+        try {
+            const quote = await Quotes.findOne({
+                order: [Sequelize.fn('RAND')],
+            });
+            res.json(quote);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Server error' });
+        }
+    },
 
-  async quoteAll(_, res) {
-    try {
-      const quotes = await Quotes.findAll();
-      res.json(quotes);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  },
+    async slackRandomQuote(_, res) {
+        const quote = await mainController.randomQuote();
+        res.json({
+            response_type: 'in_channel',
+            text: quote.content,
+            attachments: [
+                {
+                    text: `- ${quote.author}, ${quote.film}`,
+                },
+            ],
+        });
+    },
 
-  async quoteId(req, res) {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      return res.status(400).json({ error: 'Bad request' });
-    }
-    try {
-      const quote = await Quotes.findOne({
-        where: { id: id },
-      });
-      res.json(quote);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  },
+    async quoteAll(_, res) {
+        try {
+            const quotes = await Quotes.findAll();
+            res.json(quotes);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Server error' });
+        }
+    },
 
-  async persoName(req, res) {
-    const name = req.params.name;
-    if (!name) {
-      return res.status(400).json({ error: 'Bad request' });
-    }
-    try {
-      const quote = await Quotes.findOne({
-        where: { id: 29 },
-      });
-      const perso = quote.content.split('Jack').join(name);
-      res.json({ perso });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: 'Server error' });
-    }
-  },
+    async quoteId(req, res) {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ error: 'Bad request' });
+        }
+        try {
+            const quote = await Quotes.findOne({
+                where: { id: id },
+            });
+            res.json(quote);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Server error' });
+        }
+    },
 
-  async easterEgg(_, res) {
-    res.status(418).json({ error: "I'm a teapot" });
-  },
+    async persoName(req, res) {
+        const name = req.params.name;
+        if (!name) {
+            return res.status(400).json({ error: 'Bad request' });
+        }
+        try {
+            const quote = await Quotes.findOne({
+                where: { id: 29 },
+            });
+            const perso = quote.content.split('Jack').join(name);
+            res.json({ perso });
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Server error' });
+        }
+    },
+
+    async easterEgg(_, res) {
+        res.status(418).json({ error: "I'm a teapot" });
+    },
 };
 
 module.exports = mainController;
