1ebd7528d65e779d7db5350372de84d400fc41bd
kenny <kenny@nematis.com>
2024-12-20 16:45:47 +0100

    add endpoint for slack

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', mainController.mainUrl);
 
 router.get('/api/v1/quote/random', mainController.randomQuote);
-router.get('/api/v1/quote/slack_random', mainController.slackRandomQuote);
+router.post('/api/v1/quote/slack_random', mainController.slackRandomQuote);
 router.get('/api/v1/quote/all', mainController.quoteAll);
 router.get('/api/v1/quote/:id', mainController.quoteId);
 router.get('/api/v1/perso/:name', mainController.persoName);
